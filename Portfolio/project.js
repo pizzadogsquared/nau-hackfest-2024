dd4cbecd708a00c93a0f48fecd4547f7cc96c835
ebs233@nau.edu <ebs233@nau.edu>
2024-10-12 14:44:09 -0700

    First commit

---
+++ Portfolio/project.js
@@ -0,0 +1,69 @@
+let projectData = [
+    {
+        image: 'img/project-1.png',
+        name: 'project one',
+        detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, augue quis rutrum auctor, erat est mattis velit, vel luctus est nisl',
+        github: '#',
+        live: '#',
+        tags: '#javascript, #fullstack, #css'
+    },
+    {
+        image: 'img/project-2.png',
+        name: 'project two',
+        detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, augue quis rutrum auctor, erat est mattis velit, vel luctus est nisl',
+        github: '#',
+        live: '#',
+        tags: '#javascript, #css'
+    },
+    {
+        image: 'img/project-3.png',
+        name: 'project three',
+        detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, augue quis rutrum auctor, erat est mattis velit, vel luctus est nisl',
+        github: '#',
+        live: '#',
+        tags: '#javascript'
+    },
+    {
+        image: 'img/project-4.png',
+        name: 'project four',
+        detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, augue quis rutrum auctor, erat est mattis velit, vel luctus est nisl',
+        github: '#',
+        live: '#',
+        tags: '#fullstack, #css'
+    },
+    {
+        image: 'img/project-5.png',
+        name: 'project five',
+        detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, augue quis rutrum auctor, erat est mattis velit, vel luctus est nisl',
+        github: '#',
+        live: '#',
+        tags: '#fullstack'
+    },
+    {
+        image: 'img/project-6.png',
+        name: 'project six',
+        detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, augue quis rutrum auctor, erat est mattis velit, vel luctus est nisl',
+        github: '#',
+        live: '#',
+        tags: '#css'
+    },
+    {
+        image: 'img/project-7.png',
+        name: 'project seven',
+        detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, augue quis rutrum auctor, erat est mattis velit, vel luctus est nisl',
+        github: '#',
+        live: '#',
+        tags: '#javascript'
+    },
+    {
+        image: 'img/project-8.png',
+        name: 'project eight',
+        detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, augue quis rutrum auctor, erat est mattis velit, vel luctus est nisl',
+        github: '#',
+        live: '#',
+        tags: '#css'
+    },
+]
+
+// creating project cards in frontend
+
